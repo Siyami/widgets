7651d409f19286c8831810343ada69a7cd3c0f98
Siyami Avci <siyamiavci@gmail.com>
2020-11-30 21:02:49 -0800

    add Accordion

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Accordion from './Accordion';
 
-export default () => {
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a JS framework' 
+    },
+    {
+        title: 'What is React1',
+        content: 'React is a JS framework' 
+    },
+    {
+        title: 'What is React2',
+        content: 'React is a JS framework' 
+    }
+];
+
+const App = () => {
     return (
         <div>
-            <Accordion />
+            <Accordion  items={items} />
         </div>
     )
 }
+
+export default App;
