d9b45581ff85259d2eb9bd56918a89c7060650e6
Siyami Avci <siyamiavci@gmail.com>
2021-02-18 21:24:32 -0800

    add Search Component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -7,15 +7,17 @@ const Accordion = ({ items }) => {
         setActiveIndex(ind);
     }
     const renderedItems = items.map((item, ind) => {
+        const active = ind === activeIndex ? 'active' : ''; 
+
         return (
             <React.Fragment key={item.title}>
                 <div 
                     onClick={() => onTitleClick(ind)}
-                    className="title active">
+                    className={`title ${active}`}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -24,7 +26,6 @@ const Accordion = ({ items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
-        {activeIndex}
     </div>
 }
 
